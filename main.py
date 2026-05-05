cb93121a7aee032646555192e0a5c1d0ba88da6d
SteveKoko <idiazmarotom@gmail.com>
2026-05-05 12:31:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import nmap
+
+escaneo = nmap.PortScanner()
+
+while True:
+    print("[01] - Comprobar host activos en la red")
+    print("[02] - Escanear red o equipos en profundidad")
+    print("[00] - Salir")
+    print("")
+    elec = input("Elige una opcion: ")
+
+    if elec == "0":
+        print("Adiós!!")
+        break
+
+    elif elec == "1":
+        target = input("Introduce la red objetivo: ")
+        escaneo.scan(target, arguments="-T5 -sn")
+        for ip in escaneo.all_hosts():
+            print(ip)
+            print("--------------")
+    elif elec == "2":
+        target = input("Introduce el equipo o la red a escanear: ")
+        escaneo.scan(target, arguments="-T5 -p-")
+        for host in escaneo.all_hosts():
+            for prot in escaneo[host].all_protocols():
+                for port in escaneo[host][prot].keys():
+                    print(
+                        f"{host} | {prot} | {port} | {escaneo[host][prot][port]['state']}"
+                    )
+            print("--------------------")
